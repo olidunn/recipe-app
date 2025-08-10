639b655a55453605b0992097e844104e6d0fdef2
Jasper Dunn <jasperdunn93@gmail.com>
2025-08-10 16:09:40 +1000

    Reapply "Finished create user and login endpoints"
    This reverts commit 7804d18f0db4b68906b9262b187f4ca3b2977056.

---
+++ apps/client/src/pages/CreateAccount/index.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Button } from "~/components/Button";
+import { Form } from "~/components/Form";
+import { InputText } from "~/components/InputText";
+import { paths } from "~/common/routes";
+import { useLocation } from "wouter";
+
+export function CreateAccount() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [creatingAccount, setCreatingAccount] = useState(false);
+  const [, setLocation] = useLocation();
+
+  async function createAccount() {
+    try {
+      setErrorMessage("");
+      setCreatingAccount(true);
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/create-account`,
+        {
+          body: JSON.stringify({ name, email, password, confirmPassword }),
+          method: "POST",
+        }
+      );
+
+      if (response.status === 400) {
+        setErrorMessage(await response.text());
+        return;
+      }
+
+      setLocation(paths.home);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      } else {
+        setErrorMessage("We were unable to create your account.");
+      }
+    } finally {
+      setCreatingAccount(false);
+    }
+  }
+
+  return (
+    <Form>
+      <InputText
+        id="name"
+        label="Name"
+        onChange={(event) => setName(event.target.value)}
+        value={name}
+      />
+      <InputText
+        id="email"
+        label="Email"
+        onChange={(event) => setEmail(event.target.value)}
+        value={email}
+      />
+      <InputText
+        id="password"
+        label="Password"
+        onChange={(event) => setPassword(event.target.value)}
+        value={password}
+        error={errorMessage}
+      />
+      <InputText
+        id="confirmPassword"
+        label="Confirm password"
+        onChange={(event) => setConfirmPassword(event.target.value)}
+        value={confirmPassword}
+      />
+      <Button
+        style={{
+          marginLeft: "auto",
+        }}
+        onClick={createAccount}
+      >
+        {creatingAccount ? "loading..." : "Create account"}
+      </Button>
+    </Form>
+  );
+}
